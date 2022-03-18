489fe4af9dc76a5564a93036c47c06846260eaff
Tim Morgan <git@timothymorgan.info>
2022-03-18 01:00:22 -0700

    Fix tap to dismiss bug

--- Shared/Source/Views/Helpers/TapToDismiss.swift
+++ Shared/Source/Views/Helpers/TapToDismiss.swift
@@ -17,12 +17,15 @@ struct TapToDismissKeyboard: ViewModifier {
     func body(content: Content) -> some View {
         #if canImport(UIKit)
         return content.onAppear {
-            guard let window = UIApplication.shared.windows.first else { return }
-            let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
-            tapGesture.requiresExclusiveTouchType = false
-            tapGesture.cancelsTouchesInView = false
-            tapGesture.delegate = tapDelegate
-            window.addGestureRecognizer(tapGesture)
+            for scene in UIApplication.shared.connectedScenes {
+                guard let scene = scene as? UIWindowScene else { continue }
+                guard let window = scene.windows.first else { return }
+                let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+                tapGesture.requiresExclusiveTouchType = false
+                tapGesture.cancelsTouchesInView = false
+                tapGesture.delegate = tapDelegate
+                window.addGestureRecognizer(tapGesture)
+            }
         }
         #else
         return content
