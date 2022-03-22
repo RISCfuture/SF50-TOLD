8bb77a509f22df5e1bd8e9edec062f46149715f5
Tim Morgan <git@timothymorgan.info>
2022-03-22 01:17:23 -0700

    Fixes for Xcode 13.3

---
+++ Shared/Source/Extensions/Cycle+Defaults.swift
@@ -0,0 +1,35 @@
+import Foundation
+import SwiftNASR
+import Defaults
+
+public class CycleBridge: DefaultsBridge {
+    public typealias Value = Cycle
+    public typealias Serializable = String
+    
+    public func serialize(_ value: Cycle?) -> String? {
+        guard let value = value else { return nil }
+        return "\(value.year)-\(value.month)-\(value.day)"
+    }
+    
+    public func deserialize(_ object: String?) -> Cycle? {
+        guard let object = object else { return nil }
+
+        let parts = object.components(separatedBy: "-")
+        guard let year = Int(parts[0]) else { return nil }
+        guard let mon = Int(parts[1]) else { return nil }
+        guard let day = Int(parts[2]) else { return nil }
+        
+        let dateComp = DateComponents(calendar: Calendar(identifier: .gregorian), year: year, month: mon, day: day)
+        guard let date = dateComp.date else { return nil}
+        
+        return Cycle.effectiveCycle(for: date)
+    }
+}
+
+extension Cycle: DefaultsSerializable {
+    public typealias Bridge = CycleBridge
+    
+    public static var bridge: CycleBridge {
+        CycleBridge()
+    }
+}
