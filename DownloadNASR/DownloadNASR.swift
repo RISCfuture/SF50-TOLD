d1d651d80e41bc38359cf0e56913bec6140dc48c
Tim Morgan <git@timothymorgan.info>
2022-12-26 14:22:26 -0800

    Add “next” shortcut for downloading next cycle

--- DownloadNASR/DownloadNASR.swift
+++ DownloadNASR/DownloadNASR.swift
@@ -6,23 +6,17 @@ import SwiftNASR
 @main
 final class DownloadNASR: AsyncParsableCommand {
     @Option(name: .shortAndLong,
-            help: "The cycle to download.",
+            help: "The cycle to download (use “next” for the next cycle). (default: current cycle)",
             transform: { (value: String) -> Cycle in
-        let components = value.split(separator: "-")
-        guard components.count == 3 else {
+        guard let cycle = (value == "next") ? Cycle.current.next : stringToCycle(value) else {
             throw ValidationError("Invalid cycle")
         }
-        guard let year = UInt(components[0]),
-              let month = UInt8(components[1]),
-              let day = UInt8(components[2]) else {
-            throw ValidationError("Invalid cycle")
-        }
-        return Cycle(year: year, month: month, day: day)
+        return cycle
     })
     var cycle = Cycle.current
     
     @Argument(help: "The location to write the plist files. (default: current directory)",
-            transform: { URL(fileURLWithPath: $0) })
+              transform: { URL(fileURLWithPath: $0) })
     var outputLocation = URL(fileURLWithPath: "")
     
     func validate() throws {
@@ -61,4 +55,17 @@ final class DownloadNASR: AsyncParsableCommand {
         
         logger.info("Complete")
     }
+}
+
+fileprivate func stringToCycle(_ value: String) -> Cycle? {
+    let components = value.split(separator: "-")
+    guard components.count == 3 else { return nil }
+    
+    guard let year = UInt(components[0]),
+          let month = UInt8(components[1]),
+          let day = UInt8(components[2]) else {
+        return nil
+    }
+    
+    return Cycle(year: year, month: month, day: day)
 }
