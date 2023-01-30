8417db98b8d75ff40ce76fe042a7169ac91b758a
Tim Morgan <github@timothymorgan.info>
2023-01-30 13:09:35 -0800

    VisionJet -> Vision Jet

--- Shared/Source/Views/Sections/SettingsView.swift
+++ Shared/Source/Views/Sections/SettingsView.swift
@@ -8,7 +8,7 @@ struct SettingsView: View {
             Form {
                 VStack(alignment: .leading) {
                     Toggle("Use Updated Thrust Schedule", isOn: $state.updatedThrustSchedule)
-                    Text("Turn this setting on when flying a G2+ VisionJet or one with SB5X-72-01 completed.")
+                    Text("Turn this setting on when flying a G2+ Vision Jet or one with SB5X-72-01 completed.")
                         .font(.system(size: 11))
                         .fixedSize(horizontal: false, vertical: true)
                 }
