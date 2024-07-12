864683280a1419cde0938c4e18aa705080547d8d
Tim Morgan <github@timothymorgan.info>
2024-07-12 00:30:50 -0700

    Move WeatherValues to own file

--- Shared/Source/State/WeatherState.swift
+++ Shared/Source/State/WeatherState.swift
@@ -65,7 +65,7 @@ class WeatherState: ObservableObject {
     }
     
     convenience init(date: Date, observation: METAR?, forecast: TAF?) {
-        guard let values = valuesFrom(date: date, observation: observation, forecast: forecast) else {
+        guard let values = WeatherValues(date: date, observation: observation, forecast: forecast) else {
             self.init()
             return
         }
@@ -139,7 +139,7 @@ class WeatherState: ObservableObject {
                 forecastError = nil
         }
 
-        guard let values = valuesFrom(date: date, observation: observation, forecast: forecast) else {
+        guard let values = WeatherValues(date: date, observation: observation, forecast: forecast) else {
             DispatchQueue.main.async { self.resetToISA(observationError: observationError, forecastError: forecastError) }
             return
         }
@@ -162,69 +162,4 @@ class WeatherState: ObservableObject {
 
         }
     }
-}
-
-fileprivate func windFromEnum(_ wind: SwiftMETAR.Wind) -> Wind {
-    switch wind {
-        case let .direction(heading, speed, _):
-            return .init(direction: Double(heading), speed: Double(speed.measurement.converted(to: .knots).value))
-        case let .directionRange(heading, _, speed, _):
-            return .init(direction: Double(heading), speed: Double(speed.measurement.converted(to: .knots).value))
-        default: return .calm
-    }
-}
-
-fileprivate struct WeatherValues {
-    var wind: Wind
-    var temperature: Temperature
-    var altimeter: Double
-}
-
-fileprivate func valuesFrom(date: Date, observation: METAR?, forecast: TAF?) -> WeatherValues? {
-    if observation == nil && forecast == nil { return nil }
-    
-    if let observation = observation {
-        let sinceMETAR = date.timeIntervalSince(observation.date)
-        if sinceMETAR > METARUpdatePeriod {
-            guard let forecast = forecast else { return nil }
-            if !forecast.covers(date) { return nil }
-        }
-    }
-    
-    var wind = Wind.calm
-    var temperature = Temperature.ISA
-    var altimeter = standardSLP
-    
-    if let observation = observation {
-        let sinceMETAR = date.timeIntervalSince(observation.date)
-        
-        if let observedWind = observation.wind {
-            wind = windFromEnum(observedWind)
-        }
-        
-        if let observedTemp = observation.temperature {
-            temperature = .value(Double(observedTemp))
-        }
-        
-        if let observedAlt = observation.altimeter {
-            altimeter = Double(observedAlt.measurement.converted(to: .inchesOfMercury).value)
-        }
-        
-        if sinceMETAR > METARUpdatePeriod {
-            if let forecast = forecast {
-                if let group = forecast.during(date) {
-                    if let forecastWind = group.wind {
-                        wind = windFromEnum(forecastWind)
-                    }
-                    if let forecastAlt = group.altimeter {
-                        altimeter = Double(forecastAlt.measurement.converted(to: .inchesOfMercury).value)
-                    }
-                }
-            }
-        }
-    }
-    
-    return WeatherValues(wind: wind,
-                         temperature: temperature,
-                         altimeter: altimeter)
 }
