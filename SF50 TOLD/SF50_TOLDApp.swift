8863e11edee33356b8bd0a13419af5587a0e61c7
Tim Morgan <github@timothymorgan.info>
2025-10-28 15:59:01 -0700

    Update Sentry integration

--- SF50 TOLD/SF50_TOLDApp.swift
+++ SF50 TOLD/SF50_TOLDApp.swift
@@ -89,47 +89,39 @@ struct SF50_TOLDApp: App {
       UITestingHelper.setupUITestingEnvironment(container: sharedModelContainer)
     }
 
-    // Only initialize Sentry if not running UI tests
-    if !isUITesting {
-      SentrySDK.start { options in
-        options.dsn =
-          "https://18ccb9d2342467fafcaebcc33cc676e5@o4510156629475328.ingest.us.sentry.io/4510161674502144"
-        options.debug = true  // Enabled debug when first installing is always helpful
+    SentrySDK.start { options in
+      options.dsn =
+        "https://18ccb9d2342467fafcaebcc33cc676e5@o4510156629475328.ingest.us.sentry.io/4510161674502144"
+      options.debug = true  // Enabled debug when first installing is always helpful
 
-        // Adds IP for users.
-        // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
-        options.sendDefaultPii = true
+      // Adds IP for users.
+      // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+      options.sendDefaultPii = true
 
-        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-        // We recommend adjusting this value in production.
-        options.tracesSampleRate = 1.0
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0
 
-        // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
-        options.configureProfiling = {
-          $0.sessionSampleRate = 1.0  // We recommend adjusting this value in production.
-          $0.lifecycle = .trace
-        }
+      // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+      options.configureProfiling = {
+        $0.sessionSampleRate = 1.0  // We recommend adjusting this value in production.
+        $0.lifecycle = .trace
+      }
 
-        // Filter out all errors from simulators to reduce CI noise
-        options.beforeSend = { event in
-          // Check if this event is from a simulator
-          if let contexts = event.context,
-            let device = contexts["device"],
-            let simulator = device["simulator"] as? Bool,
-            simulator == true
-          {
-            // Drop all simulator errors
-            return nil
-          }
+      // Uncomment the following lines to add more data to your events
+      // options.attachScreenshot = true // This adds a screenshot to the error events
+      // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+
+      // Enable experimental logging features
+      options.experimental.enableLogs = true
+
+      // Discard all events when running on simulator
+      options.beforeSend = { event in
+        #if targetEnvironment(simulator)
+          return nil
+        #else
           return event
-        }
-
-        // Uncomment the following lines to add more data to your events
-        // options.attachScreenshot = true // This adds a screenshot to the error events
-        // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
-
-        // Enable experimental logging features
-        options.experimental.enableLogs = true
+        #endif
       }
     }
   }
